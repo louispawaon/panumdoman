81b3e7cbb3877cf53e67c813bf6510fbae700b22
louispawaon <lpawaon@gmail.com>
2023-01-14 18:29:00 +0800

    update index - implemented logic for previously shown word being a currentword within a set of conditions - problems with scoring/life mechanic of the game

--- panumdoman/pages/index.tsx
+++ panumdoman/pages/index.tsx
@@ -1,82 +1,77 @@
-import React, {useEffect,useState,useCallback} from "react";  
+import React, {useEffect,useState} from "react";  
 import tagalogWords from '../pages/corpus/wordlist.json';
 
-interface Props {
-  tagalogWords: string[];
+interface Word {
+  tagalogWords: string;
+  seen:boolean;
 }
 
 export default function Home() {
-  const [shownWord, setShownWord] = useState<string[]>([]);
-  const [currentWord, setCurrentWord] = useState<string>('');
+  const [shownWord, setShownWord] = useState<Word[]>([]);
+  const [currentWord, setCurrentWord] = useState<Word>({ tagalogWords: "", seen: false });
   const [score, setScore] = useState<number>(0);
   const [lives, setLives] = useState<number>(3);
+  const [allWords, setAllWords] = useState<string[]>(tagalogWords.words);
   const [gameOver, setGameOver] = useState<boolean>(false);
+  const [newWordCounter, setNewWordCounter] = useState<number>(0);
 
+const restartGame = () => {
+  setCurrentWord({ tagalogWords: "", seen: false });
+  setShownWord([]);
+  setScore(0);
+  setLives(3);
+  setAllWords(tagalogWords.words);
+  setGameOver(false);
+}
+
+useEffect(() => {
+  const nextWord = tagalogWords.words[Math.floor(Math.random() * tagalogWords.words.length)];
+  setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
+}, []);
+
+const handleGuess = (guess: boolean) => {
+  //scoring mechanic is my problem
+  if (guess === currentWord.seen) { //new and not seen (default false)
+      setScore(score => score + 1);
+  }
+  else if (currentWord.seen){ //seen and seen
+      setLives(lives => lives -1)
+  } 
   
-  const getRandomWord=useCallback(()=>{
-    let randomWord = tagalogWords.words[Math.floor(Math.random() * tagalogWords.words.length)];
-    /*if(shownWord.length > 10){
-      // choose a random word from shown words
-      randomWord = shownWord[Math.floor(Math.random() * shownWord.length)];
-    }*/
-    setCurrentWord(randomWord);
-    
-  },[])
+  //logic for previousword being a currentword again
+  if (shownWord.length === 0 || (shownWord.length > 0 && shownWord[shownWord.length-1].tagalogWords !== currentWord.tagalogWords)) {
+      setNewWordCounter(newWordCounter + 1);
+  }
+  setShownWord([...shownWord, currentWord]);
+  setAllWords(allWords.filter(w => w !== currentWord.tagalogWords));
 
-  const restartGame = useCallback(() => {
-    setShownWord([]);
-    setCurrentWord('');
-    setScore(0);
-    setLives(3);
-    setGameOver(false);
-    getRandomWord();
-  }, [getRandomWord]);
-
-
-  useEffect(() => {
-    getRandomWord();
-  }, [getRandomWord]);
-
-  const handleGuess=useCallback((guess:string)=> {
-    if (guess === 'NEW' && !shownWord.includes(currentWord)) {
-      setShownWord(prevWords=>[...prevWords,currentWord]);
-      // The user correctly identified a new word
-      setScore(prevScore => prevScore + 1);
-    } else if (guess === 'NEW' && shownWord.includes(currentWord)) {
-      // The user incorrectly identified a previously shown word
-      setLives(prevLives => prevLives - 1);
-    } 
-    else if (guess === 'SEEN' && shownWord.includes(currentWord)) {
-      // The user correctly identified a previously shown word
-      setScore(prevScore => prevScore + 1);
-    }
-    else if (guess === 'SEEN' && !shownWord.includes(currentWord)) {
-      // The user incorrectly identified a new word
-      setLives(prevLives => prevLives - 1);
-    }
-
-    if(lives === 0){
-      setGameOver(true);
-    }else{
-      getRandomWord()
-    }
-    },[shownWord,currentWord,lives,getRandomWord])
+  if(newWordCounter>=5 && (newWordCounter % 5 === 0 || newWordCounter % 2 === 0)) {
+      const nextWord = shownWord[Math.floor(Math.random() * shownWord.length)];
+      setCurrentWord({ tagalogWords: nextWord.tagalogWords, seen: nextWord.seen });
+      setNewWordCounter(0);
+  } else {
+      const nextWord = allWords[Math.floor(Math.random() * allWords.length)];
+      setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
+  }
+}
 
   return (
-    
     <div>
-      {gameOver?(
-        <div>Game Over!</div>):(
-        <>
-        <p>Word: {currentWord}</p>
-        <p>Score: {score}</p>
-        <p>Remaining Lives: {lives}</p>
-        <p>Have you seen this word before?</p>
-        <button onClick={() => handleGuess('SEEN')}>SEEN</button>
-        <button onClick={() => handleGuess('NEW')}>NEW</button>
-        </>
+        {gameOver ? (
+            <div>
+                <p>Game Over</p>
+                <button onClick={restartGame}>Restart</button>
+            </div>
+        ) : (
+          <div>
+              <p>Current word: {currentWord.tagalogWords}</p>
+              <p>Score: {score}</p>
+              <p>Remaining Lives: {lives}</p>
+              <p>Have you seen this word before?</p>
+              <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => handleGuess(true)}>SEEN</button>
+              <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => handleGuess(false)}>NEW</button>
+          </div> 
         )}
-        {gameOver && <button onClick={restartGame}>Restart</button>}
-    </div> 
+    </div>
   )
-  }
+}
