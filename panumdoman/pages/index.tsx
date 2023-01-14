ad70be868a7c28bc4450662bc6c4444f6239f8d5
louispawaon <lpawaon@gmail.com>
2023-01-14 10:18:40 +0800

    update index - trying to fix lives system

--- panumdoman/pages/index.tsx
+++ panumdoman/pages/index.tsx
@@ -10,53 +10,73 @@ export default function Home() {
   const [currentWord, setCurrentWord] = useState<string>('');
   const [score, setScore] = useState<number>(0);
   const [lives, setLives] = useState<number>(3);
+  const [gameOver, setGameOver] = useState<boolean>(false);
 
   
   const getRandomWord=useCallback(()=>{
-    const randomWord = tagalogWords[Math.floor(Math.random() * tagalogWords.length)];
+    let randomWord = tagalogWords[Math.floor(Math.random() * tagalogWords.length)];
+    /*if(shownWord.length > 10){
+      // choose a random word from shown words
+      randomWord = shownWord[Math.floor(Math.random() * shownWord.length)];
+    }*/
     setCurrentWord(randomWord);
-    setShownWord(prevWords=>[...prevWords,randomWord]);
-    
+    //setShownWord(prevWords=>[...prevWords,randomWord]);
   },[])
+
+
+  const restartGame = useCallback(() => {
+    setShownWord([]);
+    setCurrentWord('');
+    setScore(0);
+    setLives(3);
+    setGameOver(false);
+    getRandomWord();
+  }, [getRandomWord]);
+
 
   useEffect(() => {
     getRandomWord();
   }, [getRandomWord]);
 
   const handleGuess=useCallback((guess:string)=> {
-    if (guess === 'yes' && shownWord.includes(currentWord)) {
-      // The user correctly identified a previously shown word
+    if (guess === 'NEW' && !shownWord.includes(currentWord)) {
+      // The user correctly identified a new word
       setScore(prevScore => prevScore + 1);
-    } else if (guess === 'no' && shownWord.includes(currentWord)) {
+    } else if (guess === 'NEW' && shownWord.includes(currentWord)) {
       // The user incorrectly identified a previously shown word
       setLives(prevLives => prevLives - 1);
-    } else if (guess === 'yes' && !shownWord.includes(currentWord)) {
+    } 
+    else if (guess === 'SEEN' && shownWord.includes(currentWord)) {
+      // The user correctly identified a previously shown word
+      setScore(prevScore => prevScore + 1);
+    }
+    else if (guess === 'SEEN' && !shownWord.includes(currentWord)) {
       // The user incorrectly identified a new word
       setLives(prevLives => prevLives - 1);
     }
-    /*
-    // check if the word is seen again after certain time
-    if (wordShownTime) {
-        const timePassed = Date.now() - wordShownTime;
-        // check if word is seen again after 5 seconds
-        if (timePassed > 5000) {
-          setScore(prevScore => prevScore + 1);
-        }*/
-        setTimeout(getRandomWord, 100);
-    },[shownWord,currentWord,getRandomWord])
-    // Show a new random word
-    
+
+    if(lives === 0){
+      setGameOver(true);
+    }else{
+      getRandomWord()
+    }
+    },[shownWord,currentWord,lives,getRandomWord])
+
   return (
     
     <div>
-      <p>Word: {currentWord}</p>
-      <p>Score: {score}</p>
-      <p>Remaining Lives: {lives}</p>
-      <p>Have you seen this word before?</p>
-      <button onClick={() => handleGuess('yes')}>Yes</button>
-      <button onClick={() => handleGuess('no')}>No</button>
-    </div>
-    
-    
+      {gameOver?(
+        <div>Game Over!</div>):(
+        <>
+        <p>Word: {currentWord}</p>
+        <p>Score: {score}</p>
+        <p>Remaining Lives: {lives}</p>
+        <p>Have you seen this word before?</p>
+        <button onClick={() => handleGuess('SEEN')}>SEEN</button>
+        <button onClick={() => handleGuess('NEW')}>NEW</button>
+        </>
+        )}
+        {gameOver && <button onClick={restartGame}>Restart</button>}
+    </div> 
   )
   }
