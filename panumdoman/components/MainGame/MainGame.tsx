9c1e0ef17d7e041d4554dd54143acac082f7149c
louispawaon <lpawaon@gmail.com>
2023-02-11 22:25:18 +0800

    update MainGame - still lost sa scoring/lives mechanic - add game over if lives=0

--- panumdoman/components/MainGame/MainGame.tsx
+++ panumdoman/components/MainGame/MainGame.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from "react";  
+import React, {useEffect,useState, useRef} from "react";  
 import tagalogWords from '../../pages/corpus/wordlist.json';
 
 interface Word {
@@ -8,12 +8,13 @@ interface Word {
 
 export default function MainGame(){
     const [shownWord, setShownWord] = useState<Word[]>([]);
-  const [currentWord, setCurrentWord] = useState<Word>({ tagalogWords: "", seen: false });
-  const [score, setScore] = useState<number>(0);
-  const [lives, setLives] = useState<number>(3);
-  const [allWords, setAllWords] = useState<string[]>(tagalogWords.words);
-  const [gameOver, setGameOver] = useState<boolean>(false);
-  const [newWordCounter, setNewWordCounter] = useState<number>(0);
+    const [currentWord, setCurrentWord] = useState<Word>({ tagalogWords:"", seen: false });
+    const [score, setScore] = useState<number>(0);
+    const [lives, setLives] = useState<number>(3);
+    const [allWords, setAllWords] = useState<string[]>(tagalogWords.words);
+    const [gameOver, setGameOver] = useState<boolean>(false);
+    const [newWordCounter, setNewWordCounter] = useState<number>(1);
+    const wordFetch = useRef(false)
 
 const restartGame = () => {
   setCurrentWord({ tagalogWords: "", seen: false });
@@ -24,34 +25,50 @@ const restartGame = () => {
   setGameOver(false);
 }
 
-useEffect(() => {
-  const nextWord = tagalogWords.words[Math.floor(Math.random() * tagalogWords.words.length)];
-  setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
+const setNew=()=>{
+    const nextWord = tagalogWords.words[Math.floor(Math.random() * tagalogWords.words.length)];
+    setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
+    setShownWord([...shownWord,{tagalogWords: nextWord, seen: true}]);
+    
+}
+useEffect(() => { //pre rendered
+    if (wordFetch.current) return;
+    wordFetch.current = true;
+    setNew()
 }, []);
 
 const handleGuess = (guess: boolean) => {
+    console.log(shownWord)
+    console.log(shownWord[shownWord.length-1].tagalogWords)
+    console.log(currentWord.tagalogWords)
+    
+    /*
   //scoring mechanic is my problem
   if (guess === currentWord.seen) { //new and not seen (default false)
       setScore(score => score + 1);
   }
   else if (currentWord.seen){ //seen and seen
       setLives(lives => lives -1)
-  } 
+  } */
   
   //logic for previousword being a currentword again
-  if (shownWord.length === 0 || (shownWord.length > 0 && shownWord[shownWord.length-1].tagalogWords !== currentWord.tagalogWords)) {
+  if (shownWord.length === 0 || (shownWord.length > 0 && shownWord[shownWord.length-1].tagalogWords == currentWord.tagalogWords)) {
       setNewWordCounter(newWordCounter + 1);
   }
   setShownWord([...shownWord, currentWord]);
   setAllWords(allWords.filter(w => w !== currentWord.tagalogWords));
+  //console.log("allwords", allWords, newWordCounter)
 
   if(newWordCounter>=5 && (newWordCounter % 5 === 0 || newWordCounter % 2 === 0)) {
-      const nextWord = shownWord[Math.floor(Math.random() * shownWord.length)];
-      setCurrentWord({ tagalogWords: nextWord.tagalogWords, seen: nextWord.seen });
-      //setNewWordCounter(0);
+        //console.log("after modolo", shownWord)
+        const nextWord = shownWord[Math.floor(Math.random() * shownWord.length)];
+        //console.log("after modolos", nextWord)
+        setCurrentWord({ tagalogWords: nextWord.tagalogWords, seen: nextWord.seen });
   } else {
-      const nextWord = allWords[Math.floor(Math.random() * allWords.length)];
-      setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
+        //const nextWord = allWords[Math.floor(Math.random() * allWords.length)];
+        //setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
+        setNew()
+        //console.log("else",nextWord)
   }
 }
 
