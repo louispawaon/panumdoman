e9c785d9bc688f28ecb657bb109a87c9d23373df
louispawaon <lpawaon@gmail.com>
2023-02-12 11:19:43 +0800

    update maingame - scoring-lives mechanic ok - game over mechanic missing

--- panumdoman/components/MainGame/MainGame.tsx
+++ panumdoman/components/MainGame/MainGame.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect,useState, useRef} from "react";  
+import React, {useEffect,useState, useRef,useCallback} from "react";  
 import tagalogWords from '../../pages/corpus/wordlist.json';
 
 interface Word {
@@ -28,7 +28,10 @@ const restartGame = () => {
 const setNew=()=>{
     const nextWord = tagalogWords.words[Math.floor(Math.random() * tagalogWords.words.length)];
     setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
+    //console.log("the word is", nextWord)
+    //console.log("am i appending something?")
     setShownWord([...shownWord,{tagalogWords: nextWord, seen: true}]);
+    //console.log("the shown word is", shownWord)
     
 }
 useEffect(() => { //pre rendered
@@ -37,40 +40,47 @@ useEffect(() => { //pre rendered
     setNew()
 }, []);
 
-const handleGuess = (guess: boolean) => {
-    console.log(shownWord)
-    console.log(shownWord[shownWord.length-1].tagalogWords)
-    console.log(currentWord.tagalogWords)
-    
-    /*
-  //scoring mechanic is my problem
+
+
+const handleGuess = useCallback((guess: boolean) => {
+
+  //lives-scoring mechanic
   if (guess === currentWord.seen) { //new and not seen (default false)
-      setScore(score => score + 1);
+    setScore(score => score + 1);
   }
   else if (currentWord.seen){ //seen and seen
-      setLives(lives => lives -1)
-  } */
-  
+    setScore(score => score + 1);
+  }else{
+    setLives(lives=>lives-1);
+  }
+
   //logic for previousword being a currentword again
   if (shownWord.length === 0 || (shownWord.length > 0 && shownWord[shownWord.length-1].tagalogWords == currentWord.tagalogWords)) {
       setNewWordCounter(newWordCounter + 1);
   }
-  setShownWord([...shownWord, currentWord]);
+  //setShownWord([...shownWord, currentWord]);
   setAllWords(allWords.filter(w => w !== currentWord.tagalogWords));
   //console.log("allwords", allWords, newWordCounter)
 
   if(newWordCounter>=5 && (newWordCounter % 5 === 0 || newWordCounter % 2 === 0)) {
-        //console.log("after modolo", shownWord)
+        console.log("welcome to modolo",shownWord)
         const nextWord = shownWord[Math.floor(Math.random() * shownWord.length)];
-        //console.log("after modolos", nextWord)
+        console.log("the repeating word is", nextWord)
         setCurrentWord({ tagalogWords: nextWord.tagalogWords, seen: nextWord.seen });
+        setNewWordCounter(0)
   } else {
+        //console.log("am i setting up?")
         //const nextWord = allWords[Math.floor(Math.random() * allWords.length)];
         //setCurrentWord({ tagalogWords: nextWord, seen: shownWord.some(w => w.tagalogWords === nextWord) });
         setNew()
         //console.log("else",nextWord)
   }
-}
+
+  if(lives === 0){
+    setGameOver(true);
+  }
+
+},[lives])
 
   return (
     <div>
@@ -88,7 +98,7 @@ const handleGuess = (guess: boolean) => {
               <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => handleGuess(true)}>SEEN</button>
               <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={() => handleGuess(false)}>NEW</button>
           </div> 
-        )}
+       )}
     </div>
   )
 }
