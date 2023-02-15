8ea40c7386d0083c656eda5ac42597eff03d6642
louispawaon <lpawaon@gmail.com>
2023-02-15 16:35:19 +0800

    update modals - instmodal will show upon load - aboutmodal ok - kulang nalang details and overall design

--- panumdoman/components/Header/Header.tsx
+++ panumdoman/components/Header/Header.tsx
@@ -3,21 +3,25 @@ import {MdOutlineLeaderboard} from 'react-icons/md';
 import {RiInformationLine} from 'react-icons/ri'
 import {AiOutlineQuestionCircle} from 'react-icons/ai'
 
-export default function Header(){
+interface Header{
+  onOpenInstModal: ()=>void
+  onOpenAboutModal: ()=>void
+
+}
+
+export default function Header({onOpenInstModal,onOpenAboutModal}:Header){
      return (
         <nav className="flex items-center justify-between px-4 py-3 bg-gray-800">
         <div className="flex-1 sm:text-center text-left">
           <p className="font-lexend sm:text-3xl font-semibold text-xl text-white">panumdoman.</p>
         </div>
          <div className="flex items-center">
-            <AiOutlineQuestionCircle className="text-white h-7 w-7 fill-current mr-4"/>
-
+            <AiOutlineQuestionCircle onClick={onOpenInstModal} className="text-white h-7 w-7 fill-current mr-4"/>
+            
             <MdOutlineLeaderboard className="text-white h-7 w-7 fill-current mr-4"/>
     
-            <RiInformationLine className="text-white h-7 w-7 fill-current mr-4"/>
+            <RiInformationLine onClick={onOpenAboutModal} className="text-white h-7 w-7 fill-current mr-4"/>
          </div>
        </nav>
      );
    };
-
-   {/*to be fixed ang hamburger menu bcos yes*/}
--- panumdoman/pages/index.tsx
+++ panumdoman/pages/index.tsx
@@ -1,11 +1,36 @@
 import React, {useEffect,useState} from "react";  
-import {MainGame,Header} from '../components/CompontentIndex'
+import {MainGame,Header,InstructionModal,AboutModal} from '../components/CompontentIndex'
 
 export default function Home() {
+  const [showInstModal, setShowInstModal] = useState(false);
+  const [showAboutModal, setShowAboutModal] = useState(false);
+
+  const handleInstModal=()=>{
+    setShowInstModal(true);
+  };
+
+  const handleInstModalClose=()=>{
+    setShowInstModal(false);
+  };
+
+  const handleAboutModal=()=>{
+    setShowAboutModal(true);
+  };
+
+  const handleAboutModalClose=()=>{
+    setShowAboutModal(false);
+  };
+
+  useEffect(() =>{
+    setShowInstModal(true);
+  },[])
+
   return (
-    <div>
-      <Header/>
+    <>
+      <Header onOpenInstModal={handleInstModal} onOpenAboutModal={handleAboutModal}/>
+      <InstructionModal isOpen={showInstModal} onClose={handleInstModalClose}/>
+      <AboutModal isOpen={showAboutModal} onClose={handleAboutModalClose}/>
       <MainGame/>
-    </div>
+    </>
     )
 }
