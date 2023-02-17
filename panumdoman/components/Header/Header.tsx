263d1d75670a10ac59ab0c0dc2ceefe2fd68f865
louispawaon <lpawaon@gmail.com>
2023-02-17 09:07:40 +0800

    update header - give up nkog centering - add breakpoints for responsiveness

--- panumdoman/components/Header/Header.tsx
+++ panumdoman/components/Header/Header.tsx
@@ -12,16 +12,16 @@ interface Header{
 export default function Header({onOpenInstModal,onOpenAboutModal}:Header){
      return (
         <nav className=" fixed top-0 w-full flex items-center justify-between px-4 py-3 bg-gray-800">
-        <div className="flex-1 sm:text-center text-left">
-          <p className="font-lexend sm:text-3xl font-semibold text-xl text-white">panumdoman.</p>
+        <div className="flex">
+          <span className="font-lexend sm:text-3xl font-semibold text-xl text-white">panumdoman.</span>
         </div>
-         <div className="flex items-center">
-            <AiOutlineQuestionCircle onClick={onOpenInstModal} className="text-white h-7 w-7 fill-current mr-4"/>
-            
-            <MdOutlineLeaderboard className="text-white h-7 w-7 fill-current mr-4"/>
-    
-            <RiInformationLine onClick={onOpenAboutModal} className="text-white h-7 w-7 fill-current mr-4"/>
-         </div>
+        <div className="flex">
+          <AiOutlineQuestionCircle onClick={onOpenInstModal} className="text-white h-7 w-7 fill-current mr-4"/>
+          
+          <MdOutlineLeaderboard className="text-white h-7 w-7 fill-current mr-4"/>
+  
+          <RiInformationLine onClick={onOpenAboutModal} className="text-white h-7 w-7 fill-current mr-4"/>
+        </div>
        </nav>
      );
    };
