a07286629df52eada9a7cf7cf4e3884b01c9e94c
louispawaon <lpawaon@gmail.com>
2023-02-19 12:33:32 +0800

    update configs - updated screen sizes

--- panumdoman/tailwind.config.js
+++ panumdoman/tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: { 
+      'sm': {'max':'767px'},
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     fontFamily:{
       'forBody': ['Roboto', 'sans-serif'],
       'forTitle': ['Lexend Deca','sans-serif']
